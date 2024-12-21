682fcfa1b48e8b33591bcc6c8f2365482769a828
Will McCreery <2292923+thegreenbot@users.noreply.github.com>
2024-12-21 14:11:19 +0000

    demo cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
